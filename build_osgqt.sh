f37aaeca3a6843c41fa355c26dc06136a3402690
YngveE <Yngve.Einarsson@smhi.se>
2019-06-20 10:39:18 +0000

    make clean added

--- build_osgqt.sh
+++ build_osgqt.sh
@@ -9,6 +9,7 @@ then
 fi
 mkdir -p build
 mkdir -p local
-cd build 
+cd build
+make clean 
 cmake3 -D CMAKE_INSTALL_PREFIX=$BUILDROOT/local -DBUILD_OSG_EXAMPLES=1 -D DESIRED_QT_VERSION=4 -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_RELWITHDEBINFO_POSTFIX="" ..
 make install
